ba49d5498f21c5747f78a811b5ee6bf4d3f23332
roychenyi <852416288@qq.com>
2016-06-02 21:33:48 +0800

    friend
    Signed-off-by: roychenyi <852416288@qq.com>

---
+++ wxs/user/com/cx/wxs/service/impl/UFriendGroupServiceImpl.java
@@ -0,0 +1,69 @@
+package com.cx.wxs.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.cx.wxs.dto.UFriendDto;
+import com.cx.wxs.dto.UFriendGroupDto;
+import com.cx.wxs.service.UFriendGroupService;
+import com.cx.wxs.service.UFriendService;
+
+/**
+ * @author 陈义
+ * @date   2016-5-29 下午9:59:41
+ */
+@Service("UFriendGroupService")
+public class UFriendGroupServiceImpl implements UFriendGroupService{
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendGroupService#getUfriendGroupByID(com.cx.wxs.dto.UFriendGroupDto)
+	 */
+	@Override
+	public UFriendGroupDto getUfriendGroupByID(UFriendGroupDto ufriendGroupDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendGroupService#getUfriendGroupList(com.cx.wxs.dto.UFriendGroupDto)
+	 */
+	@Override
+	public List<UFriendGroupDto> getUfriendGroupList(
+			UFriendGroupDto ufriendGroupDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendGroupService#addUfriendGroup(com.cx.wxs.dto.UFriendGroupDto)
+	 */
+	@Override
+	public Integer addUfriendGroup(UFriendGroupDto ufriendGroupDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendGroupService#updateUfriendGroup(com.cx.wxs.dto.UFriendGroupDto)
+	 */
+	@Override
+	public Integer updateUfriendGroup(UFriendGroupDto ufriendGroupDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendGroupService#deleteUfriendByGroup(com.cx.wxs.dto.UFriendGroupDto)
+	 */
+	@Override
+	public Integer deleteUfriendByGroup(UFriendGroupDto ufriendGroupDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
---
+++ wxs/user/com/cx/wxs/service/impl/UFriendServiceImpl.java
@@ -0,0 +1,89 @@
+package com.cx.wxs.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cx.wxs.dto.UFriendDto;
+import com.cx.wxs.service.UFriendService;
+
+/**
+ * @author 陈义
+ * @date   2016-5-29 下午9:56:55
+ */
+@Service("UFriendService")
+public class UFriendServiceImpl implements UFriendService{
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#getUfriendByID(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public UFriendDto getUfriendByID(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#getUfriendList(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public List<UFriendDto> getUfriendList(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#getFriendCountByGroup(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public Integer getFriendCountByGroup(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#getFriendCountByUser(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public Integer getFriendCountByUser(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#addUfriend(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public Integer addUfriend(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#updateUfriend(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public Integer updateUfriend(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#deleteUfriend(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public Integer deleteUfriend(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.cx.wxs.service.UFriendService#deleteUfriendByGroupID(com.cx.wxs.dto.UFriendDto)
+	 */
+	@Override
+	public Integer deleteUfriendByGroupID(UFriendDto ufriendDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
