fc8ebf8727ede7d39f3b06b30da4e828da6ec124
roychenyi <852416288@qq.com>
2016-08-09 00:23:14 +0800

    动静分离0808

--- wxs/WebRoot/static/my/js/main.js
+++ wxs/WebRoot/static/my/js/main.js
@@ -291,7 +291,7 @@ function ajax1(url,data_str,success_function){
 }
 //js获取项目根路径，如： http://localhost:8083/uimcardprj
 function getRootPath(){
-    //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+  /*  //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
     var curWwwPath=window.document.location.href;
     //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
     var pathName=window.document.location.pathname;
@@ -300,7 +300,8 @@ function getRootPath(){
     var localhostPaht=curWwwPath.substring(0,pos);
     //获取带"/"的项目名，如：/uimcardprj
     var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
-    return(localhostPaht);
+    return(localhostPaht);*/
+	return homePath;
 }
 //json解析，有时候因为 fastjson生成的json数据带有$引用会出现查找不到的情况
 var FastJson={isArray:function(a){return"object"==typeof a&&"[object array]"==Object.prototype.toString.call(a).toLowerCase()},isObject:function(a){return"object"==typeof a&&"[object object]"==Object.prototype.toString.call(a).toLowerCase()},format:function(a){if(null==a)return null;"string"==typeof a&&(a=eval("("+a+")"));return this._format(a,a,null,null,null)},_randomId:function(){return"randomId_"+parseInt(1E9*Math.random())},_getJsonValue:function(a,c){var d=this._randomId(),b;b=""+("function "+d+"(root){")+("return root."+c+";");b+="}";b+="";var e=document.createElement("script");e.id=d;e.text=b;document.body.appendChild(e);d=window[d](a);e.parentNode.removeChild(e);return d},_format:function(a,c,d,b,e){d||(d="");if(this.isObject(c)){if(c.$ref){var g=c.$ref;0==g.indexOf("$.")&&(b[e]=this._getJsonValue(a,g.substring(2)));return}for(var f in c)b=d,""!=b&&(b+="."),g=c[f],b+=f,this._format(a,g,b,c,f)}else if(this.isArray(c))for(f in c)b=d,g=c[f],b=b+"["+f+"]",this._format(a,g,b,c,f);return a}};
